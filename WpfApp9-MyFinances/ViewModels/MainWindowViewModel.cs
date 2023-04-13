f6b51c1be5a68597c7cc814601af3dd45bba3d1d
Vasyl Bihan <bihanvasyl@gmail.com>
2023-04-13 23:35:54 +0300

    added categories and providers creating

--- WpfApp9-MyFinances/ViewModels/MainWindowViewModel.cs
+++ WpfApp9-MyFinances/ViewModels/MainWindowViewModel.cs
@@ -7,6 +7,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using WpfApp9_MyFinances.Models;
@@ -24,6 +25,16 @@ public class MainWindowViewModel : NotifyPropertyChangedBase
         _allPaymentMethods = new List<PaymentMethod>();
         _allCategoriesExp = new List<CategoriesExp>();
         _categoriesExpItems = new ObservableCollection<TreeViewItem>();
+        _allCategoriesInc = new List<CategoriesInc>();
+        _categoriesIncItems= new ObservableCollection<TreeViewItem>();
+        _allProviders = new List<Provider>();
+        _categoryTypes = new List<string> { "Expense", "Income" };
+        _selectedOperationType = String.Empty;
+        _titleOfNewCategory = String.Empty;
+        _validNameOfNewCategory = false;
+        _isNewCategoryASubcategory = false;
+        _titleOfNewProvider = String.Empty;
+        _validNameOfNewProvider = false;
 
         Task.Run(async () =>
         {
@@ -31,11 +42,18 @@ public class MainWindowViewModel : NotifyPropertyChangedBase
             _allPaymentMethods.ForEach(p => PaymentMethods.Add(new PaymentMethodViewModel(p)));
             _allCategoriesExp = await LoadCategoriesExpAsync();
             _allCategoriesExp.ForEach(c => CategoriesExp.Add(new CategoryExpViewModel(c)));
+            _allCategoriesInc = await LoadCategoriesIncAsync();
+            _allCategoriesInc.ForEach(c => CategoriesInc.Add(new CategoryIncViewModel(c)));
+            _allProviders = await LoadProvidersAsync();
+            _allProviders.ForEach(p => Providers.Add(new ProviderViewModel(p)));
             //_allCategoriesExp.ForEach(c => CategoriesExpItems.Add(new TreeViewItem { Header = c.Title }));
 
             OnPropertyChanged(nameof(PaymentMethods));
             OnPropertyChanged(nameof(CategoriesExp));
             OnPropertyChanged(nameof(CategoriesExpItems));
+            OnPropertyChanged(nameof(CategoriesInc));
+            OnPropertyChanged(nameof(CategoriesIncItems));
+            OnPropertyChanged(nameof(Providers));
             OnPropertyChanged(nameof(TotalInCash));
             OnPropertyChanged(nameof(TotalInCashless));
             OnPropertyChanged(nameof(TotalMoney));
@@ -50,6 +68,14 @@ public class MainWindowViewModel : NotifyPropertyChangedBase
     public async Task<List<CategoriesExp>> LoadCategoriesExpAsync()
     {
         return await _db.CategoriesExps.Include(x=> x.SubcategoriesExps).ToListAsync();
+    }
+    public async Task<List<CategoriesInc>> LoadCategoriesIncAsync()
+    {
+        return await _db.CategoriesIncs.ToListAsync();
+    }
+    public async Task<List<Provider>> LoadProvidersAsync()
+    {
+        return await _db.Providers.ToListAsync();
     }
     private List<PaymentMethod> _allPaymentMethods;
 
@@ -117,6 +143,254 @@ public class MainWindowViewModel : NotifyPropertyChangedBase
             //OnPropertyChanged(nameof(CategoriesExp));
         }
     }
+    private List<CategoriesInc> _allCategoriesInc;
+    public ObservableCollection<CategoryIncViewModel> CategoriesInc
+    {
+        get
+        {
+            var collection = new ObservableCollection<CategoryIncViewModel>();
+            foreach (var category in _allCategoriesInc)
+            {
+                collection.Add(new CategoryIncViewModel(category));
+            }
+            return collection;
+        }
+        set
+        {
+            CategoriesInc = value;
+            OnPropertyChanged(nameof(CategoriesExp));
+            //OnPropertyChanged(nameof(CategoriesExpItems));
+        }
+    }
+    private ObservableCollection<TreeViewItem> _categoriesIncItems = new ObservableCollection<TreeViewItem>();
+    public ObservableCollection<TreeViewItem> CategoriesIncItems
+    {
+        get
+        {
+            // FIX no subcategory in category
+            var collection = new ObservableCollection<TreeViewItem>();
+            int i = 0;
+            _allCategoriesInc.ForEach(c =>
+            {
+                collection.Add(new TreeViewItem { Header = c.Title });
+
+                //if (c.SubcategoriesExps.Count > 0)//  c.SubcategoriesExps!=null)
+                //{
+                //    c.SubcategoriesExps.ToList().ForEach(s => collection[i].Items.Add(new TreeViewItem { Header = s.Title }));
+                //}
+                i++;
+            });
+            return collection;
+            //return _categoriesExpItems;
+        }
+        set
+        {
+            _categoriesIncItems = value;
+            OnPropertyChanged(nameof(CategoriesIncItems));
+            //OnPropertyChanged(nameof(CategoriesExp));
+        }
+    }
+    private List<string> _categoryTypes;
+    public ObservableCollection<string> CategoryTypes
+    {
+        get
+        {
+            var collection = new ObservableCollection<string>();
+            _categoryTypes.ForEach(collection.Add);
+            return collection;
+        }
+    }
+    private List<Provider> _allProviders;
+    public ObservableCollection<ProviderViewModel> Providers
+    {
+        get
+        {
+            var collection = new ObservableCollection<ProviderViewModel>();
+            _allProviders.ForEach(p => collection.Add(new ProviderViewModel(p)));
+            return collection;
+        }
+        set
+        {
+            Providers= value;
+            OnPropertyChanged(nameof(Providers));
+        }
+    }
+    private string _selectedOperationType;
+    public string SelectedOperationType
+    {
+        get => _selectedOperationType;
+        set
+        {
+            _selectedOperationType = value;
+            OnPropertyChanged(nameof(SelectedOperationType));
+            OnPropertyChanged(nameof(IsCategoryTypeExp));
+        }
+    }
+    private string _titleOfNewCategory;
+    public string TitleOfNewCategory
+    {
+        get=> _titleOfNewCategory;
+        set
+        {
+            _titleOfNewCategory = value;
+            OnPropertyChanged(nameof(TitleOfNewCategory));
+        }
+    }
+    private CategoryExpViewModel _selectedMainCategoryExpForSub;
+    public CategoryExpViewModel SelectedMainCategoryExpForSub
+    {
+        get => _selectedMainCategoryExpForSub;
+        set
+        {
+            _selectedMainCategoryExpForSub = value;
+            OnPropertyChanged(nameof(SelectedMainCategoryExpForSub));
+        }
+    }
+    public bool IsCategoryTypeExp
+    {
+        get
+        {
+            var res = _selectedOperationType.Equals("Expense");
+            if (!res)
+                IsNewCategoryASubcategory = false;
+            return res;
+        }
+        set
+        {
+            IsCategoryTypeExp = value;
+            OnPropertyChanged(nameof(IsCategoryTypeExp));
+            OnPropertyChanged(nameof(IsNewCategoryASubcategory));
+        }
+    }
+    public ICommand CheckTitleOfNewCategory => new RelayCommand(x =>
+    {
+        if (SelectedOperationType.Equals("Expense") && !(_isNewCategoryASubcategory))
+        {
+            var newCat = _allCategoriesExp.FirstOrDefault(x => x.Title.ToLower().Equals(_titleOfNewCategory.ToLower()));
+            if (newCat != null)
+            {
+                MessageBox.Show("Such title already exist", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                MessageBox.Show("Title is unique", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                _validNameOfNewCategory = true;
+            }
+        }
+        else if(SelectedOperationType.Equals("Expense") && _isNewCategoryASubcategory)
+        {
+            var mainCat = _allCategoriesExp.FirstOrDefault(x => x.Title.ToLower().Equals(SelectedMainCategoryExpForSub.Title.ToLower()));
+            if(mainCat != null)
+            {
+                var newSubCat = mainCat.SubcategoriesExps.FirstOrDefault(x => x.Title.Equals(_titleOfNewCategory));
+                if (newSubCat != null)
+                {
+                    MessageBox.Show("Such title already exist", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Title is unique", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                    _validNameOfNewCategory = true;
+                }
+            }
+        }
+        else
+        {
+            var newCat = _allCategoriesInc.FirstOrDefault(x => x.Title.ToLower().Equals(_titleOfNewCategory.ToLower()));
+            if (newCat != null)
+            {
+                MessageBox.Show("Such title already exist", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                MessageBox.Show("Title is unique", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                _validNameOfNewCategory = true;
+            }
+        }
+    }, x => SelectedOperationType != null && _titleOfNewCategory.Length>0);
+    public ICommand AddNewCategory => new RelayCommand(x =>
+    {
+        if (SelectedOperationType.Equals("Expense") && !(_isNewCategoryASubcategory))
+        {
+            var newCategory = new CategoriesExp { Title = _titleOfNewCategory };
+            _db.CategoriesExps.Add(newCategory);
+        }
+        else if (SelectedOperationType.Equals("Expense") && _isNewCategoryASubcategory)
+        {
+            var mainCat = _allCategoriesExp.FirstOrDefault(x => x.Title.Equals(SelectedMainCategoryExpForSub.Title));
+            if(mainCat != null)
+            {
+                mainCat.SubcategoriesExps.Add(new SubcategoriesExp { Title = _titleOfNewCategory, CategoryId = mainCat.Id });
+            } 
+        }
+        else
+        {
+            var newCategory = new CategoriesInc { Title = _titleOfNewCategory };
+            _db.CategoriesIncs.Add(newCategory);    
+        }
+        _db.SaveChanges();
+
+    }, x => _validNameOfNewCategory);
+    private bool _validNameOfNewCategory;
+    public bool ValidNameOfNewCategory
+    {
+        get=> _validNameOfNewCategory;
+        set
+        {
+            _validNameOfNewCategory= value;
+            OnPropertyChanged(nameof(ValidNameOfNewCategory));
+        }
+    }
+    private bool _isNewCategoryASubcategory;
+    public bool IsNewCategoryASubcategory
+    {
+        get => _isNewCategoryASubcategory;
+        set
+        {
+            _isNewCategoryASubcategory= value;
+            OnPropertyChanged(nameof(IsNewCategoryASubcategory));
+        }
+    }
+    private string _titleOfNewProvider;
+    public string TitleOfNewProvider
+    {
+        get => _titleOfNewProvider;
+        set
+        {
+            _titleOfNewProvider = value;
+            OnPropertyChanged(nameof(TitleOfNewProvider));
+        }
+    }
+    private bool _validNameOfNewProvider;
+    public bool ValidNameOfNewProvider
+    {
+        get => _validNameOfNewProvider;
+        set
+        {
+            _validNameOfNewProvider = value;
+            OnPropertyChanged(nameof(ValidNameOfNewProvider));
+        }
+    }
+    public ICommand CheckTitleOfNewProvider => new RelayCommand(x =>
+    {
+        var newProvider = _allProviders.FirstOrDefault(x => x.Title.ToLower().Equals(_titleOfNewProvider.ToLower()));
+        if(newProvider != null)
+        {
+            MessageBox.Show("Such title already exist", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        else
+        {
+            MessageBox.Show("Title is unique", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            ValidNameOfNewProvider = true;
+        }
+    }, x => _titleOfNewProvider.Length > 0);
+    public ICommand AddNewProvider => new RelayCommand(x =>
+    {
+        var newProvider = new Provider { Title = _titleOfNewProvider };
+        _db.Providers.Add(newProvider);
+        _db.SaveChanges();
+
+    }, x => ValidNameOfNewProvider);
     public decimal TotalInCash
     {
         get
